cb63175993b1c7a3a6ffea62b9294800f659ca5b
Andrei Home <andrei.preoteasa.ap@gmail.com>
2022-04-07 18:37:25 +0300

    updated gitignore

--- calorie-counter/src/personcreation.py
+++ calorie-counter/src/personcreation.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sqlite3
 
 class userdata():
@@ -9,14 +8,20 @@ class userdata():
         self.db.isolation_level = None
 
         self.db.execute("create table User (name TEXT, age INTEGER, sex INTEGER, height REAL, weight REAL)")
+        self.db.execute("create table Meals (date TEXT, description TEXT, calories REAL)")
+        self.db.execute("create table Exercises (date TEXT, description TEXT, calories REAL)")
     
     def NewPerson(self, name, age, sex, height, weight):
         self.db.isolation_level = None
         self.db.execute(f"insert into User (name, age, sex, height, weight) values ('{name}', {age}, {sex}, {height}, {weight})")
 
+    def NewMeal(self, date, description, calories):
+        self.db.isolation_level = None
+        self.db.execute(f"insert into Meals (date, description, calories) values ('{date}', '{description}', '{calories}')")
+
+    def NewExercise(self, date, description, calories):
+        self.db.isolation_level = None
+        self.db.execute(f"insert into Exercises (date, description, calories) values ('{date}', '{description}', '{calories}')")
+
 if __name__ == "__main__":
     u = userdata()
-=======
-from sqlite3 import db
-
->>>>>>> bb56f654b57392dff0a2a35cfcfa814602639aa3
