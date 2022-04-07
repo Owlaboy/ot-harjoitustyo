cfa4e52ce8639bb322296b31191d65edeedffe87
Andrei Home <andrei.preoteasa.ap@gmail.com>
2022-04-07 17:33:11 +0300

    basic database implementation

--- calorie-counter/dokumentaatio/tuntikirjanpito.md
+++ calorie-counter/dokumentaatio/tuntikirjanpito.md
@@ -4,3 +4,4 @@
 | :----:|:-----| :-----|
 |29.03.2022|2|Vaatimusmääirittelyn ideointi ja luonti|
 |04.04.2022|3|Projektin aloittaminen, testien alustava luonti, invoke-tehtävät luotu|
+|07.04.2022|1|Luotu tietokannan alustaja, mihin tallennetaan läyttäjän perus tiedot|
---
+++ calorie-counter/src/personcreation.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+class userdata():
+    def __init__(self):
+        with open("user.db", "w") as u:
+            pass
+        self.db = sqlite3.connect("user.db")
+        self.db.isolation_level = None
+
+        self.db.execute("create table User (name TEXT, age INTEGER, sex INTEGER, height REAL, weight REAL)")
+    
+    def NewPerson(self, name, age, sex, height, weight):
+        self.db.isolation_level = None
+        self.db.execute(f"insert into User (name, age, sex, height, weight) values ('{name}', {age}, {sex}, {height}, {weight})")
+
+if __name__ == "__main__":
+    u = userdata()
